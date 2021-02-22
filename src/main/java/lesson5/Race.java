a04f857fca5acd84a1119fcbdf638064124201a0
tlenparty <tevez-32@inbox.ru>
2021-02-22 13:23:05 +0300

    initial commit

---
+++ src/main/java/lesson5/Race.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    public ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages(){
+        return stages;
+    }
+    public Race(Stage...stages){
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+
+}
